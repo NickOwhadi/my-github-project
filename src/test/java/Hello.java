61c9f399ce923aca523b472bdf41244038e9e83c
n7owhad <n7owhad@hotmail.com>
2022-02-13 02:56:08 -0500

    some update added Hello.java

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!!!!!");
         System.out.println("try again");
+        System.out.println("try again");
     }
 }
