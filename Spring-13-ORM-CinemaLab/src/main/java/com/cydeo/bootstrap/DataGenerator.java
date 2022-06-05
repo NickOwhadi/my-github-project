b5bdeea004345e49945072a75ecd6a873a01d0d9
n7owhad <n7owhad@hotmail.com>
2022-06-05 03:10:30 -0400

    API

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,6 +1,7 @@
 package com.cydeo.bootstrap;
 
 import com.cydeo.entity.Account;
+import com.cydeo.enums.UserRole;
 import com.cydeo.repository.AccountRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -18,8 +19,34 @@ public class DataGenerator implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
-
+        System.out.println("-------------Account Derived Country or State--------------");
+        accountRepository.findByCountryOrState(null, "New York").forEach(System.out::println);
+        System.out.println("-------------Account Derived Age less than or equal--------------");
+        accountRepository.findByAgeLessThanEqual(35).forEach(System.out::println);
+        System.out.println("-------------Account Derived Role--------------");
+        accountRepository.findByRole(UserRole.USER).forEach(System.out::println);
+        System.out.println("-------------Account Derived Age Between--------------");
+        accountRepository.findByAgeBetween(20,46).forEach(System.out::println);
+        System.out.println("-------------Account Derived Address Start With--------------");
+        accountRepository.findByAddressStartingWith("2").forEach(System.out::println);
+        System.out.println("-------------Account Derived Age Sorted--------------");
+        //accountRepository.findByAgeBetweenOrderByAgeDesc(0,100).forEach(System.out::println);
+        System.out.println("-------------Account Derived--------------");
+        //accountRepository.findByAgeIsNotNullOrderByAgeDesc().forEach(System.out::println);
+        accountRepository.findByOrderByAgeDesc().forEach(System.out::println);
+        System.out.println("---------------------------Account JPQL QUERIES All Accounts ---------------------------------");
+        accountRepository.getAllAccounts().forEach(System.out::println);
+        System.out.println("---------------------------Account JPQL QUERIES ADMIN Role ---------------------------------");
+        accountRepository.getAllAdminAccounts(UserRole.ADMIN).forEach(System.out::println);
+        System.out.println("---------------------------Account JPQL QUERIES All Account sorted with Age  ---------------------------------");
+        accountRepository.getAllWithAge().forEach(System.out::println);
+        System.out.println("---------------------------Account Native QUERIES age less than ---------------------------------");
+        accountRepository.readALlAccountWithAgeLowerThan(46).forEach(System.out::println);
+        System.out.println("---------------------------Account Native QUERIES ---------------------------------");
+        accountRepository.readALlAccountWithAgeLowerThan(48).forEach(System.out::println);
+        System.out.println("---------------------------Account JPQL QUERIES ---------------------------------");
+        System.out.println("---------------------------Account JPQL QUERIES ---------------------------------");
+        System.out.println("---------------------------Account JPQL QUERIES ---------------------------------");
 
     }
 }
--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/AccountRepository.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/AccountRepository.java
@@ -1,8 +1,13 @@
 package com.cydeo.repository;
 
 import com.cydeo.entity.Account;
+import com.cydeo.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
@@ -10,31 +15,42 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     // ------------------- DERIVED QUERIES ------------------- //
 
     //Write a derived query to list all accounts with a specific country or state
-
+    List<Account> findByCountryOrState(String country, String State);
     //Write a derived query to list all accounts with age lower than or equal to a specific value
-
+    List<Account> findByAgeLessThanEqual(int age);
     //Write a derived query to list all accounts with a specific role
-
+    List<Account> findByRole(UserRole role);
     //Write a derived query to list all accounts between a range of ages
-
+    List<Account> findByAgeBetween(int startAge, int endAge);
     //Write a derived query to list all accounts where the beginning of the address contains the keyword
-
+    List<Account> findByAddressStartingWith(String startKeyword);
     //Write a derived query to sort the list of accounts with age
-
+    List<Account> findByOrderByAgeDesc();
+    List<Account> findByAgeBetweenOrderByAgeDesc(int intAge, int endAge);
+    List<Account> findByAgeIsNotNullOrderByAgeDesc();
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query that returns all accounts
-
+    @Query("SELECT a FROM Account a")
+    List<Account>  getAllAccounts();
     //Write a JPQL query to list all admin accounts
-
+    @Query("SELECT a FROM Account a WHERE a.role = ?1")
+    List<Account>  getAllAdminAccounts(UserRole role);
     //Write a JPQL query to sort all accounts with age
-
+    @Query("SELECT a FROM Account a WHERE a.age is NOT NULL ORDER BY a.age DESC ")
+    List<Account>  getAllWithAge();
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query to read all accounts with an age lower than a specific value
-
+    @Query(value = "select * from account_details where age < ?1",nativeQuery = true)
+    List<Account> readALlAccountWithAgeLowerThan(@Param("age") Integer age);
     //Write a native query to read all accounts that a specific value can be containable in the name, address, country, state city
-
+    @Query(value = "SELECT * FROM account_details WHERE name ILIKE concat('%', ?1, '%') " +
+            "OR country ILIKE concat('%', ?1, '%') " +
+            "OR address ILIKE concat('%', ?1, '%') " +
+            "OR state ILIKE concat('%', ?1, '%') " +
+            "OR city ILIKE concat('%', ?1, '%') ", nativeQuery = true)
+    List<Account> retrieveBySearchCriteria(@Param("pattern") String pattern);
     //Write a native query to read all accounts with an age lower than a specific value
 
 }
